c5537d995c3cd47137c45106b0d57841c5ed1284
Simon Tower <simon@klein-homepage.de>
2022-12-12 23:36:07 +0100

    added ICE/USDT farming on XDC

--- packages/farms/constants/50.ts
+++ packages/farms/constants/50.ts
@@ -7,13 +7,20 @@ const farms: SerializedFarmConfig[] = [
         lpSymbol: 'ICE-XDC LP',
         lpAddress: '0x1b42Bd5464812930146E78d5DAa69CF8669e463b',
         token: xdcTokens.ice,
-        quoteToken: xdcTokens.usdt,
+        quoteToken: xdcTokens.wxdc,
     },
     {
         pid: 1,
         lpSymbol: 'USDT-XDC LP',
         lpAddress: '0xe9450d66a493C3ae6eBC3Bb0B2B01a5107ea8bDb',
         token: xdcTokens.wxdc,
+        quoteToken: xdcTokens.usdt,
+    },
+    {
+        pid: 2,
+        lpSymbol: 'ICE-USDT LP',
+        lpAddress: '0x76629B41825f342519139D8Be7B05F38C0E7422c',
+        token: xdcTokens.ice,
         quoteToken: xdcTokens.usdt,
     }
 ].map((p) => ({ ...p, token: p.token.serialize, quoteToken: p.quoteToken.serialize }))
