13c0a26b5600de854da762e1ead53060a17232bc
Simon Tower <simon@klein-homepage.de>
2023-02-01 21:26:11 +0100

    added ICE and Miidas project link

--- packages/tokens/src/56.ts
+++ packages/tokens/src/56.ts
@@ -3,7 +3,7 @@ import { ChainId, WBNB, ERC20Token } from '@pancakeswap/sdk'
 export const bscTokens = {
   usdt: new ERC20Token(ChainId.BSC, '0x55d398326f99059ff775485246999027b3197955', 18, 'USDT', 'Tether USD'),
   usdti: new ERC20Token(ChainId.BSC, '0x55d398326f99059ff775485246999027b3197955', 18, 'USDT', 'Tether USD'),
-  ice: new ERC20Token(ChainId.BSC, '0xce6c9c70f91c6797873EFC80505f972290A88f5D', 18, 'ICE', 'IceToken'),
+  ice: new ERC20Token(ChainId.BSC, '0xce6c9c70f91c6797873EFC80505f972290A88f5D', 18, 'ICE', 'IceToken', 'https://icecreamswap.com'),
   wbnb: WBNB[ChainId.BSC],
   bnb: new ERC20Token(
     ChainId.BSC,
