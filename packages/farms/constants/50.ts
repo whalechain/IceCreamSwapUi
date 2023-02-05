f779f093b77e5b5af3fcb062e94e27880acf3bd9
Simon Tower <simon@klein-homepage.de>
2023-02-05 22:42:03 +0100

    fixed XDC farm APY calculation by changing the farms order

--- packages/farms/constants/50.ts
+++ packages/farms/constants/50.ts
@@ -3,13 +3,6 @@ import { xdcTokens } from '@pancakeswap/tokens'
 
 const farms: SerializedFarmConfig[] = [
     {
-        pid: 0,
-        lpSymbol: 'ICE-XDC LP',
-        lpAddress: '0x1b42Bd5464812930146E78d5DAa69CF8669e463b',
-        token: xdcTokens.ice,
-        quoteToken: xdcTokens.wxdc,
-    },
-    {
         pid: 2,
         lpSymbol: 'ICE-USDT LP',
         lpAddress: '0x76629B41825f342519139D8Be7B05F38C0E7422c',
@@ -20,7 +13,14 @@ const farms: SerializedFarmConfig[] = [
         pid: 1,
         lpSymbol: 'USDT-XDC LP',
         lpAddress: '0xe9450d66a493C3ae6eBC3Bb0B2B01a5107ea8bDb',
-        token: xdcTokens.usdt,
+        token: xdcTokens.wxdc,
+        quoteToken: xdcTokens.usdt,
+    },
+    {
+        pid: 0,
+        lpSymbol: 'ICE-XDC LP',
+        lpAddress: '0x1b42Bd5464812930146E78d5DAa69CF8669e463b',
+        token: xdcTokens.ice,
         quoteToken: xdcTokens.wxdc,
     },
     {
