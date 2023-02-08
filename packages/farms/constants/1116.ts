f96cff4f3980deed10b7fb116a48a9034fd20f3d
Simon Tower <simon@klein-homepage.de>
2023-02-08 10:16:44 +0100

    set up farms, but not taking them live yet.

--- packages/farms/constants/1116.ts
+++ packages/farms/constants/1116.ts
@@ -1,6 +1,14 @@
 import { SerializedFarmConfig } from '@pancakeswap/farms'
+import { coreTokens } from '@pancakeswap/tokens'
 
 const farms: SerializedFarmConfig[] = [
+    {
+        pid: 0,
+        lpSymbol: 'ICE-USDT LP',
+        lpAddress: '0xf1a996efba43dcbd7945d2b91fa78420d9c23bf0',
+        token: coreTokens.ice,
+        quoteToken: coreTokens.usdt,
+    },
 ].map((p) => ({ ...p, token: p.token.serialize, quoteToken: p.quoteToken.serialize }))
 
 export default farms
