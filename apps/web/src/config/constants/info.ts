2afe91cdd7d946e89ed2bcd32a5398c214df2896
Simon Tower <simon@klein-homepage.de>
2023-02-14 00:10:57 +0100

    added basic CORE and XDC Info page support

--- apps/web/src/config/constants/info.ts
+++ apps/web/src/config/constants/info.ts
@@ -8,6 +8,8 @@ export const TREASURY_FEE = 0.0005
 export const BUYBACK_FEE = 0.
 
 export const PCS_BITGERT_START = 1654207200 // June 03, 2022
+export const PCS_XDC_START = 1670108400 // Dec 04, 2022
+export const PCS_CORE_START = 1675828800 // Feb 08, 2023
 export const ONE_DAY_UNIX = 86400 // 24h * 60m * 60s
 export const ONE_HOUR_SECONDS = 3600
 
