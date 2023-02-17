2cbd18c28c6d8ce61f48f37ab639df8a261ecaab
Simon Tower <simon@klein-homepage.de>
2023-02-17 17:39:12 +0100

    removed Doken from Bridge

--- apps/web/src/config/constants/supportChains.ts
+++ apps/web/src/config/constants/supportChains.ts
@@ -8,6 +8,6 @@ export const SUPPORT_ONLY_BITGERT = [ChainId.BITGERT]
 export const SUPPORT_SWAP = [ChainId.BITGERT, ChainId.DOKEN, ChainId.FUSE, ChainId.DOGE, ChainId.XDC, ChainId.CORE]
 export const SUPPORT_STAKING = [ChainId.BITGERT, ChainId.XDC]
 export const SUPPORT_INFO = [ChainId.BITGERT]
-export const SUPPORT_BRIDGE = [ChainId.BITGERT, ChainId.DOKEN, ChainId.FUSE, ChainId.DOGE, ChainId.XDC, ChainId.BSC, ChainId.CORE]
+export const SUPPORT_BRIDGE = [ChainId.BITGERT, ChainId.FUSE, ChainId.DOGE, ChainId.XDC, ChainId.BSC, ChainId.CORE]
 
 export const SUPPORT_ZAP = []
