f38aea89303a9ea52c3dd7ba9c864d43a5ab58dd
MasterGordon <gologo.x@gmail.com>
2023-02-20 07:30:55 +0100

    fixed supported chains

--- apps/web/src/pages/locks/index.tsx
+++ apps/web/src/pages/locks/index.tsx
@@ -1,7 +1,10 @@
+import { SUPPORT_LOCKS } from 'config/constants/supportChains'
 import { LocksOverview } from '../../views/Locks'
 
-const LocksOverviewPage: React.FC = () => {
+const LocksOverviewPage = () => {
   return <LocksOverview />
 }
+
+LocksOverviewPage.chains = SUPPORT_LOCKS
 
 export default LocksOverviewPage
