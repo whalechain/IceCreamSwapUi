491d2f04e35eeef908d88df1662d3718e3d5f582
Simon Tower <simon@klein-homepage.de>
2023-02-21 19:58:17 +0100

    reenabled staking for CORE

--- packages/constants/src/chains/core.ts
+++ packages/constants/src/chains/core.ts
@@ -4,7 +4,7 @@ import IceChain from '../ice-chain'
 export const core: IceChain = {
   id: 1116,
   name: 'Core Blockchain',
-  features: ['swap', 'bridge', 'farms', 'info', 'locks'],
+  features: ['swap', 'bridge', 'farms', 'info', 'locks', 'staking'],
   network: 'core',
   rpcUrls: {
     public: 'https://rpc.coredao.org/',
