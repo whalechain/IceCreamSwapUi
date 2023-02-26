530806552e9ab9043b2e3b88a5dc95ef96ea0ce6
MasterGordon <gologo.x@gmail.com>
2023-02-26 19:25:02 +0100

    dummy

--- apps/web/src/pages/api/list-users.ts
+++ apps/web/src/pages/api/list-users.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from '@prisma/client'
 
 const client = new PrismaClient()
 
-export default async function handler() {
+export default async function handler(req, res) {
   const users = await client.user.findMany()
 
   res.json(users)
