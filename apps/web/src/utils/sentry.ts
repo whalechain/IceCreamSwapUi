fd2b13ba8b5fc997451383108047afdd2cd535dc
MohammadSaeed Ghadiri <ghadirimohammadsaeed@gmail.com>
2023-05-11 11:51:29 +0430

    fix sentry.ts

--- apps/web/src/utils/sentry.ts
+++ apps/web/src/utils/sentry.ts
@@ -1,24 +1,19 @@
 import { captureException } from '@sentry/nextjs'
-import { log } from 'console'
 
 const assignError = (maybeError: any) => {
   if (typeof maybeError === 'string') {
     return new Error(maybeError)
   }
-
   if (typeof maybeError === 'object') {
     const error = new Error(maybeError?.message ?? String(maybeError))
-
     if (maybeError?.stack) {
       error.stack = maybeError.stack
     }
     if (maybeError?.code) {
       error.name = maybeError.code
     }
-
     return error
   }
-
   return maybeError
 }
 
