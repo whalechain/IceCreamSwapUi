9aa945437afa6e26955d8e7b76d00abb4f085b28
Oscar Guertler <oscarguertler@gmail.com>
2023-06-10 10:47:59 +0200

    feat: update urls and add info

--- apps/web/src/config/constants/endpoints.ts
+++ apps/web/src/config/constants/endpoints.ts
@@ -3,8 +3,8 @@ import { ChainId } from '@pancakeswap/sdk'
 export const INFO_CLIENT_BITGERT = 'https://graph-bitgert.icecreamswap.com/subgraphs/name/simone1999/icecreamswap-bitgert'
 export const BLOCKS_CLIENT_BITGERT = 'https://graph-bitgert.icecreamswap.com/subgraphs/name/icecreamswap/blocks-bitgert'
 
-export const INFO_CLIENT_XDC = 'https://graph-xdc.icecreamswap.com/subgraphs/name/simone1999/icecreamswap-xdc'
-export const BLOCKS_CLIENT_XDC = 'https://graph-xdc.icecreamswap.com/subgraphs/name/icecreamswap/blocks-xdc'
+export const INFO_CLIENT_XDC = 'https://graph-xdc.icecreamswap.dev/subgraphs/name/icecreamswap/exchange-xdc'
+export const BLOCKS_CLIENT_XDC = 'https://graph-xdc.icecreamswap.dev/subgraphs/name/icecreamswap/blocks-xdc'
 
 export const INFO_CLIENT_CORE = 'https://graph-core.icecreamswap.com/subgraphs/name/simone1999/icecreamswap-core'
 export const BLOCKS_CLIENT_CORE = 'https://graph-core.icecreamswap.com/subgraphs/name/icecreamswap/blocks-core'
