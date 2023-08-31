0301b2afe6f54cbdf1f298ee10654aab6dbaf8df
MasterGordon <gologo.x@gmail.com>
2023-08-31 22:37:59 +0200

    added user token listing

--- packages/backend/src/server/routers/token.ts
+++ packages/backend/src/server/routers/token.ts
@@ -1,9 +1,14 @@
 import { z } from 'zod'
-import { isMod } from '../session'
+import { isKyc, isMod } from '../session'
 import { publicProcedure, router } from '../trpc'
 import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3'
 import { prisma } from '../prisma'
 import { Listed, Token } from '@icecreamswap/database'
+import v2factoryAbi from '../../abi/v2factory.json'
+import v2pairAbi from '../../abi/v2pair.json'
+import { Contract, providers, utils } from 'ethers'
+import { getChain } from '@icecreamswap/constants'
+import { ICE, USD } from '@pancakeswap/tokens'
 
 export const tokenRouter = router({
   add: publicProcedure
@@ -18,7 +23,16 @@ export const tokenRouter = router({
       }),
     )
     .mutation(async ({ input, ctx }) => {
-      if (!ctx.session?.user || !isMod(ctx.session.user)) {
+      if (!ctx.session?.user) {
+        throw new Error('Unauthorized')
+      }
+      if (
+        !isKyc(ctx.session.user) ||
+        !checkDelegate(input.tokenAddress, ctx.session.user.wallet) ||
+        !(await checkLiquidity(input.tokenAddress, input.chainId))
+      ) {
+        throw new Error('Unauthorized')
+      } else if (!isMod(ctx.session.user)) {
         throw new Error('Unauthorized')
       }
       if (input.logo) {
@@ -96,3 +110,57 @@ export const tokenRouter = router({
     }
   }),
 })
+
+const checkDelegate = async (tokenAddress: string, wallet: string) => {
+  const kyc = await prisma.delegation.findFirst({
+    where: {
+      target: {
+        equals: tokenAddress,
+        mode: 'insensitive',
+      },
+      status: 'MINTED',
+      source: {
+        address: {
+          equals: wallet,
+          mode: 'insensitive',
+        },
+      },
+    },
+  })
+  return !!kyc
+}
+
+const usdThreshold = 3000
+
+const checkLiquidity = async (tokenAddress: string, chainId: number) => {
+  const chain = getChain(chainId)
+  if (!chain) {
+    return false
+  }
+  const provider = new providers.JsonRpcProvider(chain.rpcUrls.default)
+  const iceAddress = ICE[chainId].address
+  const usdAddress = USD[chainId].address
+  const iceUsdPair = new Contract(await getPairAddress(iceAddress, usdAddress, chainId), v2pairAbi, provider)
+  const tokenIcePair = new Contract(await getPairAddress(tokenAddress, iceAddress, chainId), v2pairAbi, provider)
+
+  const iceReserves = await iceUsdPair.getReserves()
+  const usdPerIce = utils.parseUnits('1', 18).mul(iceReserves[0]).div(iceReserves[1])
+
+  const tokenReserves = await tokenIcePair.getReserves()
+
+  const iceValue = usdPerIce.mul(tokenReserves[1]).div(utils.parseUnits('1', 18))
+  const liquidity = Number(utils.formatUnits(iceValue, 18)) * 2
+
+  return liquidity >= usdThreshold
+}
+
+const getPairAddress = async (tokenA: string, tokenB: string, chainId: number) => {
+  const chain = getChain(chainId)
+  if (!chain || !chain.swap?.factoryAddress) {
+    throw new Error('Invalid chainId')
+  }
+  const provider = new providers.JsonRpcProvider(chain.rpcUrls.default)
+  const factory = new Contract(chain.swap?.factoryAddress, v2factoryAbi, provider)
+  const pairAddress = await factory.getPair(tokenA, tokenB)
+  return pairAddress
+}
