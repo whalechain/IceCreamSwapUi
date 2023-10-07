4f229c1fcb79886c40f905b2ea3ed56e4ab4c5d8
simon <simon@klein-homepage.de>
2023-10-07 19:17:12 +0300

    changed average shimmer block interval for APY calculations

--- packages/constants/src/chains/shimmer.ts
+++ packages/constants/src/chains/shimmer.ts
@@ -22,7 +22,7 @@ export const shimmer: IceChain = {
     address: '0xf3a3dAf360161B2f10c645EF039C709A3Fd4Ea62',
     blockCreated: 35400,
   },
-  blockInterval: 0.5,
+  blockInterval: 3,
   wrappedNative: {
     address: '0xBEb654A116aeEf764988DF0C6B4bf67CC869D01b',
     decimals: 18,
