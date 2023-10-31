0d97f6a00402e3a3b3efabeea46fabc2e3f32218
simon <simon@klein-homepage.de>
2023-10-31 13:22:54 +0300

    corrected fees from Pancake 0.25% to Uniswap 0.3% for visual correctness

--- apps/web/src/config/constants/exchange.ts
+++ apps/web/src/config/constants/exchange.ts
@@ -161,7 +161,7 @@ export const BETTER_TRADE_LESS_HOPS_THRESHOLD = new Percent(50n, BIPS_BASE)
 export const ZERO_PERCENT = new Percent('0')
 export const ONE_HUNDRED_PERCENT = new Percent('1')
 
-export const BASE_FEE = new Percent(25n, BIPS_BASE)
+export const BASE_FEE = new Percent(30n, BIPS_BASE)
 export const INPUT_FRACTION_AFTER_FEE = ONE_HUNDRED_PERCENT.subtract(BASE_FEE)
 
 // BNB
