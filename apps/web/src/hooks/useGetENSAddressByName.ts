1611a69d6b8d380a87416e161089b8336293f377
simon <simon@klein-homepage.de>
2024-02-27 18:16:33 +0200

    added SpaceId web3 domain name service.

--- apps/web/src/hooks/useGetENSAddressByName.ts
+++ apps/web/src/hooks/useGetENSAddressByName.ts
@@ -1,6 +1,7 @@
-import { ChainId } from '@pancakeswap/sdk'
 import { useActiveChainId } from 'hooks/useActiveChainId'
 import { useEnsAddress } from 'wagmi'
+import {useEffect, useState} from "react"
+import { createWeb3Name } from '@web3-name-sdk/core'
 
 const ENS_NAME_REGEX = /^[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)?$/
 
@@ -8,6 +9,8 @@ const ADDRESS_REGEX = /^0x[a-fA-F0-9]{40}$/
 
 export const useGetENSAddressByName = (ensNameOrAddress: string) => {
   const { chainId } = useActiveChainId()
+  const [spaceIdAddress, setSpaceIdAddress] = useState<string>()
+
   const { data: recipientENSAddress } = useEnsAddress({
     name: ensNameOrAddress,
     chainId,
@@ -15,5 +18,18 @@ export const useGetENSAddressByName = (ensNameOrAddress: string) => {
       (ENS_NAME_REGEX.test(ensNameOrAddress) || ADDRESS_REGEX.test(ensNameOrAddress)) &&
       false
   })
-  return recipientENSAddress
+
+  useEffect(() => {
+    const fetchWeb3Name = async () => {
+      const web3name = createWeb3Name()
+      const address: string = await web3name.getAddress(ensNameOrAddress)
+      setSpaceIdAddress(address)
+    }
+    fetchWeb3Name().catch(reason => {
+      console.error("Error in fetching web3 domain name from SpaceId", reason)
+      setSpaceIdAddress(undefined)
+    })
+  }, [ensNameOrAddress])
+
+  return recipientENSAddress || spaceIdAddress
 }
