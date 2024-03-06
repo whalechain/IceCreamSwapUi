da974ddb75c5d551787b49a85e654eed53b421f4
simon <simon@klein-homepage.de>
2024-03-06 19:04:37 +0200

    added Qitmeer chian

---
+++ packages/constants/src/chains/qitmeer.ts
@@ -0,0 +1,38 @@
+import IceChain from '../ice-chain'
+
+export const qitmeer: IceChain = {
+  id: 813,
+  name: 'Qitmeer Blockchain',
+  features: ['swap', 'bridge'],
+  network: 'qitmeer',
+  rpcUrls: {
+    public: { http: ['https://mainnet.meerlabs.com',] },
+    default: { http: ['https://mainnet.meerlabs.com',] },
+  },
+  blockExplorers: {
+    default: { name: 'Qitmeer Explorer', url: 'https://qng.qitmeer.io' },
+  },
+  nativeCurrency: {
+    name: 'Qitmeer',
+    symbol: 'MEER',
+    decimals: 18,
+  },
+  contracts: {
+    multicall3: {
+      address: '0x42B7f4bB1a9359836942521D1d8a7Bc5d06E76f9',
+      blockCreated: 1341056,
+    },
+  },
+  blockInterval: 5,
+  wrappedNative: {
+    address: '0x470cBFB236860eb5257bBF78715FB5bd77119C2F',
+    decimals: 18,
+    symbol: 'WMEER',
+    name: 'Wrapped MEER',
+  },
+  swap: {
+    factoryAddress: "0x84aeB58fb9187dD64282d0C0975F788e21dd4475",
+    initCodeHash: "0xb41a03f1db88f3709f0a02439f5bc24ebb946603078d7fb1a50525c0c66ab827",
+    routerAddress: "0x575C065Bf1Fa9D6F0F94AAC620a6936dD8517c7D",
+  },
+}
