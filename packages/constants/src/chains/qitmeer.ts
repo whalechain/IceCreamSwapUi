36b3f0307ffe7c31d3f3e480d32860f461a10766
simon <simon@klein-homepage.de>
2024-03-24 17:16:37 +0200

    updated Qitmeer RPC

--- packages/constants/src/chains/qitmeer.ts
+++ packages/constants/src/chains/qitmeer.ts
@@ -6,8 +6,8 @@ export const qitmeer: IceChain = {
   features: ['swap', 'bridge', 'info'],
   network: 'qitmeer',
   rpcUrls: {
-    public: { http: ['https://mainnet.meerlabs.com',] },
-    default: { http: ['https://mainnet.meerlabs.com',] },
+    public: { http: ['https://rpc.dimai.ai',] },
+    default: { http: ['https://rpc.dimai.ai',] },
   },
   blockExplorers: {
     default: { name: 'Qitmeer Explorer', url: 'https://qng.qitmeer.io' },
