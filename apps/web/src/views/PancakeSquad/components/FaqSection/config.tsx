4122f8438bc84049bfed6da43b6de8635fd2daed
need47 <need47@gmail.com>
2024-04-13 11:23:32 -0400

    translation for AI branding

--- apps/web/src/views/PancakeSquad/components/FaqSection/config.tsx
+++ apps/web/src/views/PancakeSquad/components/FaqSection/config.tsx
@@ -33,25 +33,25 @@ const config = ({ t }: FAQsType) => [
     title: t('How does IceCreamSwap combine AI with Blockchains?'),
     description: [
       t('The Blockchain is an amazing tool for security and trust minimization. But as it\'s still in It\'s early stage, ' +
-      'inefficiencies occur which result in an unwanted value transfer from users to MEV/Arbitrage bots. ' +
-      'Our AI DEX aggregator detects these inefficiencies and automatically extracts them for our users, resulting in more efficient trades and less value lost.'),
+        'inefficiencies occur which result in an unwanted value transfer from users to MEV/Arbitrage bots. ' +
+        'Our AI DEX aggregator detects these inefficiencies and automatically extracts them for our users, resulting in more efficient trades and less value lost.'),
     ],
   },
   {
     title: t('What is the IceCreamSwap AI DEX aggregator?'),
     description: [
-      t('It \'s a sophisticated system that scans the entire blockchain to find the optimal way to execute your swap. ' +
-      'It allows you to trade any token at the best rate, not only taking into account the IceCreamSwap liquidity, but also the liquidity of other DEXes. ' +
-      'Simplified you can think about it as the booking.com of DEXes, finding you the best offer to execute your trade. ' +
-      'It even extracts Arbitrage opportunities for you, so your trades sometimes end with more value than they began with.'),
+      t('It\'s a sophisticated system that scans the entire blockchain to find the optimal way to execute your swap. ' +
+        'It allows you to trade any token at the best rate, not only taking into account the IceCreamSwap liquidity, but also the liquidity of other DEXes. ' +
+        'Simplified you can think about it as the booking.com of DEXes, finding you the best offer to execute your trade. ' +
+        'It even extracts Arbitrage opportunities for you, so your trades sometimes end with more value than they began with.'),
     ],
   },
   {
     title: t('How does IceCreamSwap extract arbitrage for me?'),
     description: [
       t('On every trade, our AI DEX aggregator not only finds you the best way to execute that trade, but even scans for Arbitrage opportunities. ' +
-      'These opportunities are extracted directly in your trade. ' +
-      'As most Arbitrage is generated from inefficient trades, IceCreamSwap users not only don\'t loose value to Arbitrage, they even directly profit from users that still use less sophisticated DEXes.'),
+        'These opportunities are extracted directly in your trade. ' +
+        'As most Arbitrage is generated from inefficient trades, IceCreamSwap users not only don\'t loose value to Arbitrage, they even directly profit from users that still use less sophisticated DEXes.'),
     ],
   },
   {
