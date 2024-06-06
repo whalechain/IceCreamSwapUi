5273845463473f4a4afd214b3987267f9e19892f
powman <paulophpweb@gmail.com>
2024-06-06 14:00:54 -0300

    Update

--- packages/swap-sdk/src/entities/pair.ts
+++ packages/swap-sdk/src/entities/pair.ts
@@ -94,8 +94,8 @@ export class Pair {
       tokenAmounts[0].currency.chainId,
       Pair.getAddress(tokenAmounts[0].currency, tokenAmounts[1].currency),
       18,
-        'ICELP',
-        'icecreamswap.com LP'
+        'WHALELP',
+        'whalechain.live LP'
     )
     this.tokenAmounts = tokenAmounts as [CurrencyAmount<ERC20Token>, CurrencyAmount<ERC20Token>]
   }
