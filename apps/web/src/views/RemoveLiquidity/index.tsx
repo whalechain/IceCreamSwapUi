446c9f45ce0122696fc14b60a811e556e41fe4c2
powman <paulophpweb@gmail.com>
2024-06-06 13:55:12 -0300

    Update

--- apps/web/src/views/RemoveLiquidity/index.tsx
+++ apps/web/src/views/RemoveLiquidity/index.tsx
@@ -163,7 +163,7 @@ export default function RemoveLiquidity({ currencyA, currencyB, currencyIdA, cur
       { name: 'verifyingContract', type: 'address' },
     ]
     const domain = {
-      name: 'whalechain.live LP',
+      name: 'icecreamswap.com LP',
       version: '1',
       chainId,
       verifyingContract: pair.liquidityToken.address as `0x${string}`,
