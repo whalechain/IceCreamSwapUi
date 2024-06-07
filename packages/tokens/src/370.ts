3838b588facbd266f5501687d253f1433eb5a7fd
powman <paulophpweb@gmail.com>
2024-06-07 10:20:29 -0300

    Update 370.ts

--- packages/tokens/src/370.ts
+++ packages/tokens/src/370.ts
@@ -1,5 +1,7 @@
-import { ChainId, WETH9 } from '@pancakeswap/sdk'
+import { ChainId, ERC20Token, WETH9 } from '@pancakeswap/sdk'
 
 export const coreTokens = {
   wcore: WETH9[ChainId.CORE],
+  wcore_old: new ERC20Token(ChainId.CORE, '0x40375C92d9FAf44d2f9db9Bd9ba41a3317a2404f', 18, 'WCORE', 'WrappedCoreOld'),
+  core: new ERC20Token(ChainId.CORE, '0x40375C92d9FAf44d2f9db9Bd9ba41a3317a2404f',18,'CORE','CORE'),
 }
